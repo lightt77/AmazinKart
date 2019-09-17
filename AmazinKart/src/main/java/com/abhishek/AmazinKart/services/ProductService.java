7d510ba9e9674e345b7d61d646ad4a309ae300d4
lightt77 <abhisheksatam17@gmail.com>
2019-09-17 18:47:01 +0530

    Preetify json output

--- AmazinKart/src/main/java/com/abhishek/AmazinKart/services/ProductService.java
+++ AmazinKart/src/main/java/com/abhishek/AmazinKart/services/ProductService.java
@@ -2,6 +2,7 @@ package com.abhishek.AmazinKart.services;
 
 import com.abhishek.AmazinKart.models.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -34,11 +35,17 @@ public class ProductService
             product.setCurrency("INR");
             discountService.applyDiscounts(product, promotionSet);
         });
-
-        (new ObjectMapper()).writeValue(new File("./output/output.json"), products);
+        writeToJsonFile(products);
     }
 
-    public List<Product> getProducts()
+    private void writeToJsonFile(List<Product> products) throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.writeValue(new File("./output/output.json"), products);
+    }
+
+    private List<Product> getProducts()
     {
         return Arrays.stream((Objects.requireNonNull(restTemplate.getForEntity(PRODUCT_DETAILS_URL, Product[].class).getBody()))).collect(Collectors.toList());
     }
