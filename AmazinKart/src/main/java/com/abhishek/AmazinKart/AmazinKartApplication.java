a5b95e7f0ce68c3c7e626297be35f23ca253db8c
lightt77 <abhisheksatam17@gmail.com>
2019-09-17 19:12:44 +0530

    Log exceptions

--- AmazinKart/src/main/java/com/abhishek/AmazinKart/AmazinKartApplication.java
+++ AmazinKart/src/main/java/com/abhishek/AmazinKart/AmazinKartApplication.java
@@ -6,19 +6,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class AmazinKartApplication implements CommandLineRunner
 {
     @Autowired
     private ProductService productService;
+    private static final Logger LOG = Logger.getLogger(AmazinKartApplication.class.getName());
 
     public static void main(String[] args)
     {
-        SpringApplication app = new SpringApplication(AmazinKartApplication.class);
-        if (args.length > 0)
-            app.run(args[0]);
-        else
-            app.run("promotionSetA");       // defaults to set A
+        try
+        {
+            SpringApplication app = new SpringApplication(AmazinKartApplication.class);
+            if (args.length > 0)
+                app.run(args[0]);
+            else
+                app.run("promotionSetA");       // defaults to set A
+        }
+        catch (Exception ex)
+        {
+            LOG.log(Level.SEVERE, "Exception", ex);
+        }
     }
 
     @Override
