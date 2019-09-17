7539e502e32a602813d750b4d8cd9456e594269c
lightt77 <abhisheksatam17@gmail.com>
2019-09-17 18:35:09 +0530

    Ignore null values while serialising product list

--- AmazinKart/src/main/java/com/abhishek/AmazinKart/models/Product.java
+++ AmazinKart/src/main/java/com/abhishek/AmazinKart/models/Product.java
@@ -1,5 +1,8 @@
 package com.abhishek.AmazinKart.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Product
 {
     private String category;
