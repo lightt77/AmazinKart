1c92b03b7a08132704e1ce0739cffb3e047721c3
lightt77 <abhisheksatam17@gmail.com>
2019-09-17 11:06:12 +0530

    Add initial spring boot project setup

---
+++ AmazinKart/src/main/java/com/abhishek/AmazinKart/AmazinKartApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.AmazinKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazinKartApplication.class, args);
+	}
+
+}
