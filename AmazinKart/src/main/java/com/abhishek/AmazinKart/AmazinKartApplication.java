9a5bfd492ce7ef6738d713bcd980ca33a32bf6c8
lightt77 <abhisheksatam17@gmail.com>
2019-09-17 11:27:05 +0530

    Implement CommandLineRunner in main class for spring boot command line app

--- AmazinKart/src/main/java/com/abhishek/AmazinKart/AmazinKartApplication.java
+++ AmazinKart/src/main/java/com/abhishek/AmazinKart/AmazinKartApplication.java
@@ -1,13 +1,23 @@
 package com.abhishek.AmazinKart;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmazinKartApplication {
+public class AmazinKartApplication implements CommandLineRunner
+{
+    public static void main(String[] args)
+    {
+        SpringApplication app = new SpringApplication(AmazinKartApplication.class);
+        if (args.length > 0)
+            app.run(args[0]);
+        else
+            app.run("promotionSetA");       // defaults to set A
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmazinKartApplication.class, args);
-	}
-
+    @Override
+    public void run(String... args) throws Exception
+    {
+    }
 }
